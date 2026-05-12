b44de353bed597278271771206472f2291ecf9e5
riyajaiswal5607096-rgb <riya.jaiswal.5607096@ves.ac.in>
2026-05-12 22:13:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", function(){
+
+    let navbar =
+    document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add("scrolled");
+    }
+
+    else{
+
+        navbar.classList.remove("scrolled");
+    }
+});
